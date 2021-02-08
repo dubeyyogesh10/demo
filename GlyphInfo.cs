9b8e629c120b5b8481a4a3eb88b5168bc654bf73
dubeyyogesh10 <39595733+dubeyyogesh10@users.noreply.github.com>
2021-02-08 16:46:48 +0530

    Create GlyphInfo.cs

---
+++ GlyphInfo.cs
@@ -0,0 +1,12 @@
+namespace Telerik.Windows.Examples.AutoSuggestBox.FirstLook
+{
+    public class GlyphInfo
+    {
+        public object Content { get; set; }
+        public string TextContent { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string UnicodePoint { get; set; }
+        public string Description { get; set; }
+    }
+}
